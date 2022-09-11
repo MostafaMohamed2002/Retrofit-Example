e49d33e5d600ad3cf9873792ddd761ad533cfc88
mostafa-kubuntu <Mostafa0Mohamed2002@gmail.com>
2022-09-11 03:30:01 +0200

    creat instance of our api in main activity

--- app/src/main/java/com/mostafadev/retrofit_tutorialseu/MainActivity.kt
+++ app/src/main/java/com/mostafadev/retrofit_tutorialseu/MainActivity.kt
@@ -2,10 +2,14 @@ package com.mostafadev.retrofit_tutorialseu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mostafadev.retrofit_tutorialseu.remote.RetrofitInstance
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val api=RetrofitInstance.charactersApi.getCharacter("1")
+
     }
 }
