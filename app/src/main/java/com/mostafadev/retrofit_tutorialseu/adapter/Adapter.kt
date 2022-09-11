5c5c94d0fb53c270e04c7f0acebbe11bb8fb8801
mostafa-kubuntu <Mostafa0Mohamed2002@gmail.com>
2022-09-11 06:34:24 +0200

    finish Adapter.kt

--- app/src/main/java/com/mostafadev/retrofit_tutorialseu/adapter/Adapter.kt
+++ app/src/main/java/com/mostafadev/retrofit_tutorialseu/adapter/Adapter.kt
@@ -1,26 +1,43 @@
 package com.mostafadev.retrofit_tutorialseu.adapter
 
+import android.media.Image
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.transform.CircleCropTransformation
+import com.bumptech.glide.Glide
+import com.mostafadev.retrofit_tutorialseu.R
 import com.mostafadev.retrofit_tutorialseu.model.Character
 import com.mostafadev.retrofit_tutorialseu.model.CharacterResponse
 import kotlinx.android.synthetic.main.item.view.*
 
-class Adapter(private val character:List<CharacterResponse>):RecyclerView.Adapter<Adapter.ViewHolder>() {
+class Adapter(private val character:List<Character>):RecyclerView.Adapter<Adapter.ViewHolder>() {
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bindData(character: Character) {
             itemView.textView.text=character.name
-            itemView.imageView=
+             val image=itemView.imageView
+
+
+            Glide.with(itemView)
+                .load(character.image)
+                .circleCrop()
+                .into(image)
+
+
+
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item,parent,false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.itemView
+        holder.bindData(character[position])
+
     }
 
     override fun getItemCount(): Int {
