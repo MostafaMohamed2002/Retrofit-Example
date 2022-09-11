1afba2055d111f75d0015e6d19e3cabdf84306d7
mostafa-kubuntu <Mostafa0Mohamed2002@gmail.com>
2022-09-11 03:34:23 +0200

    check if response Successful by logcat

--- app/src/main/java/com/mostafadev/retrofit_tutorialseu/MainActivity.kt
+++ app/src/main/java/com/mostafadev/retrofit_tutorialseu/MainActivity.kt
@@ -2,12 +2,14 @@ package com.mostafadev.retrofit_tutorialseu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.mostafadev.retrofit_tutorialseu.model.Response
 import com.mostafadev.retrofit_tutorialseu.remote.RetrofitInstance
 import retrofit2.Call
 import retrofit2.Callback
 
 class MainActivity : AppCompatActivity() {
+    val Tag="MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,8 +17,10 @@ class MainActivity : AppCompatActivity() {
         val api=RetrofitInstance.charactersApi.getCharacter("1")
         api.enqueue(object : Callback<Response> {
             override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
-                TODO("Not yet implemented")
-            }
+if(response.isSuccessful){
+    Log.i(Tag,"Successful+${response.body()!!}")
+}
+}
 
             override fun onFailure(call: Call<Response>, t: Throwable) {
                 TODO("Not yet implemented")
