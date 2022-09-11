82b63ee0fc95ef7bafb21fb792000c18b2810cd1
mostafa-kubuntu <Mostafa0Mohamed2002@gmail.com>
2022-09-11 03:28:42 +0200

    add CharactersApi.kt Query

--- app/src/main/java/com/mostafadev/retrofit_tutorialseu/remote/CharactersApi.kt
+++ app/src/main/java/com/mostafadev/retrofit_tutorialseu/remote/CharactersApi.kt
@@ -3,8 +3,9 @@ package com.mostafadev.retrofit_tutorialseu.remote
 import com.mostafadev.retrofit_tutorialseu.model.Response
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CharactersApi {
     @GET("character")
-    fun getCharacter():Call<Response>
+    fun getCharacter(@Query("page")page:String):retrofit2.Call<Response>
 }
