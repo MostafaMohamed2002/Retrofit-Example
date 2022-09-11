14fe5df515d35cbec44d45831e9aa07716a2f0b6
mostafa-kubuntu <Mostafa0Mohamed2002@gmail.com>
2022-09-11 03:31:22 +0200

    implement enqueue method

--- app/src/main/java/com/mostafadev/retrofit_tutorialseu/MainActivity.kt
+++ app/src/main/java/com/mostafadev/retrofit_tutorialseu/MainActivity.kt
@@ -2,7 +2,10 @@ package com.mostafadev.retrofit_tutorialseu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mostafadev.retrofit_tutorialseu.model.Response
 import com.mostafadev.retrofit_tutorialseu.remote.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +13,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val api=RetrofitInstance.charactersApi.getCharacter("1")
+        api.enqueue(object : Callback<Response> {
+            override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
+                TODO("Not yet implemented")
+            }
 
+            override fun onFailure(call: Call<Response>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
